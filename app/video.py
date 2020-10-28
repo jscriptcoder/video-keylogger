6c4b6d67201b58a58f5bcd538f934937abb47e61
Francisco Ramos <jscriptcoder@gmail.com>
2020-10-28 22:25:06 +0100

    Initial commit

---
+++ app/video.py
@@ -0,0 +1,12 @@
+import cv2
+
+class Video():
+    def __init__(self):
+        self.video = cv2.VideoCapture('Atari-2600_Space-Invaders.mp4')
+
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
