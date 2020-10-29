7cdabb6b5e3a9950ff9100a00f2db59dd5b6bfe3
Francisco Ramos <jscriptcoder@gmail.com>
2020-10-29 13:59:37 +0100

    wip: video streaming done

--- app/VideoStream.py
+++ app/VideoStream.py
@@ -2,11 +2,12 @@ import cv2
 
 class VideoStream():
     def __init__(self):
-        self.video = cv2.VideoCapture('Atari-2600_Space-Invaders.mp4')
+        self.video = cv2.VideoCapture('app/Atari-2600_Space-Invaders.mp4')
 
     def __del__(self):
         self.video.release()
     
     def get_frame(self):
-        ret, jpeg = cv2.imencode('.jpg', image)
+        success, frame = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
